0d690ba0d95a937ef534be0206a2be2ff88a2f1d
adanaryss <166547499+adanaryss@users.noreply.github.com>
2024-04-26 17:10:42 -0300

    Update lista.c

--- src/lista.c
+++ src/lista.c
@@ -1,6 +1,55 @@
-/**
- * Insira seu código para solucionar o trabalho neste arquivo.
- */
-
 #include "lista.h"
+
+void Lista_new(Lista* lista, int data_size, void (*free_data)(void*)) {
+    lista->inicio = NULL;
+    lista->data_size = data_size;
+    lista->free_data = free_data;
+    lista->tamanho = 0;
+}
+
+void Lista_delete(Lista* lista) {
+    ListaNodo* atual = lista->inicio;
+    while (atual != NULL) {
+        ListaNodo* prox = atual->next;
+        if (lista->free_data != NULL)
+            lista->free_data(atual->valor);
+        free(atual);
+        atual = prox;
+    }
+    lista->inicio = NULL;
+    lista->tamanho = 0;
+}
+
+int Lista_isEmpty(Lista* lista) {
+    return lista->tamanho == 0;
+}
+
+int Lista_size(Lista* lista) {
+    return lista->tamanho;
+}
+
+void Lista_pushFront(Lista* lista, void* valor) {
+    ListaNodo* novo_no = malloc(sizeof(ListaNodo));
+    novo_no->valor = valor;
+    novo_no->next = lista->inicio;
+    lista->inicio = novo_no;
+    lista->tamanho++;
+}
+
+void Lista_pushBack(Lista* lista, void* valor) {
+    ListaNodo* novo_no = malloc(sizeof(ListaNodo));
+    novo_no->valor = valor;
+    novo_no->next = NULL;
+    if (lista->inicio == NULL) {
+        lista->inicio = novo_no;
+    } else {
+        ListaNodo* atual = lista->inicio;
+        while (atual->next != NULL) {
+            atual = atual->next;
+        }
+        atual->next = novo_no;
+    }
+    lista->tamanho++;
+}
+
 
